3f5232998596c0b1bdcd14428ac9848a289bfb5e
Pr3D3v <80762339+Pr3D3v@users.noreply.github.com>
2021-03-16 18:49:54 +0530

    Add files via upload

---
+++ tables/main.cs
@@ -0,0 +1,39 @@
+using System;
+
+
+class MainClass {
+ 
+ public static void Main (string[] args)
+ {
+
+ Console.WriteLine ("tables");
+
+Console.WriteLine("enter the number of the table you want to see");
+	
+int m = Convert.ToInt32(Console.ReadLine());
+	
+Console.WriteLine("enter the number how many times table you want to see");
+	
+int n = Convert.ToInt32(Console.ReadLine());
+	
+Console.WriteLine(m + " times table");
+	
+for (int i = 0; i < n+1; i++) 
+   
+  {
+  
+   Console.WriteLine(m + " * " + i + " = " + m*i);
+  
+   }    
+			
+string a = Console.ReadLine();
+
+Console.WriteLine(a);
+			
+  
+}
+}
+//string input
+//string d = Console.ReadLine();
+//int input
+//int e = Convert.ToInt32(Console.ReadLine());
